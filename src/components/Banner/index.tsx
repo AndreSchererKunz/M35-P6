f375fa6a1f757817356782437f8e55997ee9e57a
André Scherer Kunz <andreschererkunzt@gmail.com>
2025-03-09 15:47:24 -0300

    27-Melhorias no código

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,16 +1,13 @@
-import { Imagem, Título, Preços } from './styles'
-
 import Tag from '../Tag'
 import Button from '../Button'
-import { useEffect, useState } from 'react'
-import { Game } from '../../pages/Home'
 
 import { formataPreco } from '../ProductsList'
-
 import { useGetFeaturedGameQuery } from '../../services/api'
 
+import { Imagem, Título, Preços } from './styles'
+
 const Banner = () => {
-  const { data: game, isLoading } = useGetFeaturedGameQuery()
+  const { data: game } = useGetFeaturedGameQuery()
 
   if (!game) {
     return <h3>Carregando...</h3>
